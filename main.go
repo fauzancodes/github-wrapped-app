56202e33fe7b307f74a71c6abe1f40cc978a29c2
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2025-01-18 22:00:45 +0700

    maximized start

--- main.go
+++ main.go
@@ -20,9 +20,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "github-wrapped-app",
-		Width:  1024,
-		Height: 768,
+		Title:            "github-wrapped-app",
+		Width:            1024,
+		Height:           768,
+		WindowStartState: options.Maximised,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
